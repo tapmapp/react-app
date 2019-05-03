4e8d10ea155c64f54cd8f47b9a6160b87dae95bd
Francisco Roca <frrocaguilar@gmail.com>
2019-05-03 20:31:47 +0800

    class based components

--- src/App.js
+++ src/App.js
@@ -3,24 +3,44 @@ import React from 'react';
 import Joke from './Joke';
 import jokesData from './jokesData';
 
-function App() {
+class Header extends React.Component {
+  render() {
+    return (
+      <header>This is my header {this.props.username}</header>
+    )
+  }
+}
 
-  const jokeComponents = jokesData.map(joke => 
+class App extends React.Component {
+
+  constructor() {
+    
+  }
+
+  myJokes() {
+    return jokesData.map(joke => 
       <Joke 
         key={joke.id}
         name={joke.title} 
         desc={joke.desc}
         imgUrl="http://placekitten.com/300/400"
       />
-  );
+    );
+  }
 
-  return (
-    <div className="App">
-      <h1>This is my App Component and inside I have my Todo Item Component</h1>
-      {jokeComponents}
-    </div>
-  );
-  
-}
+  render() {
+
+    const jokeComponents = this.myJokes();
+
+    return (
+      <div className="App">
+        <Header username="Francisco Roca"/>
+        <h1>This is my App Component and inside I have my Todo Item Component</h1>
+        {jokeComponents}
+      </div>
+    )
+  }
+
+} 
 
 export default App;
